2f2fd1f70f4c2fa93105a0631160b38d3e1c5f54
patel reema <reemapatel8189@gmail.com>
2024-06-10 13:24:30 +0530

    '10/06/24-type'

---
+++ CH-4/4.1/EXPLICIT.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{       int a=65;
+	float pi=3.14;
+
+	clrscr();
+
+	printf("A:%d\n",a);
+	printf("A:%f",(float)a);
+
+	printf("\n\n");
+
+	printf("A:%.2f\n",pi);
+	printf("A:%d",(int)pi);
+
+	getch();
+
+}
---
+++ CH-4/4.1/IMPLICIT.BAK
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{       int a =65;
+
+	clrscr();
+
+	printf("A:%d\n",a);
+	printf("A:%c",a);
+
+	getch();
+
+}
