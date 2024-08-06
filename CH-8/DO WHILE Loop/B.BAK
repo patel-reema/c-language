8577bf5b16d03aa93bc8da3fb589637cc026d660
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-08-06 12:38:04 +0530

    CH-9

---
+++ CH-8/DO WHILE Loop/B.BAK
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i;
+	clrscr();
+
+	for(i=1; i<=10; i++)
+	{
+		printf("%d\t",i);
+
+		if(i==9)
+		{
+			break;
+		}
+	}
+
+	getch();
+}
---
+++ CH-8/DO WHILE Loop/C.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i;
+	clrscr();
+
+	for(i=1; i<=10; i++)
+	{
+		if(i%2!=0)
+		{
+			continue;
+		}
+		printf("%d\t",i);
+
+	}
+
+	getch();
+}
